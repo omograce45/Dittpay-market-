c9482a168395eb19e42ad16ca29ce9abc2596189
omograce45 <omograce45@gmail.com>
2025-10-29 07:10:14 +0100

    Change server port from 5000 to 10000

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const crypto = require("crypto");
 const axios = require("axios");
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(cors());
